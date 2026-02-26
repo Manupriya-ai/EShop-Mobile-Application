c1b42d94e0c84294d80f45a9f19fec8ac882f5df
Thiwanka Nirmal Manupriya <151936811+Manupriya-ai@users.noreply.github.com>
2026-02-26 23:35:24 +0530

    cart & Process to shipping
    Cart complete
    Process to shipping half complete

--- app/src/main/java/lk/tnm/eshop/fragment/CartFragment.java
+++ app/src/main/java/lk/tnm/eshop/fragment/CartFragment.java
@@ -10,18 +10,25 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 import lk.tnm.eshop.R;
 import lk.tnm.eshop.adapter.CartAdapter;
 import lk.tnm.eshop.databinding.FragmentCartBinding;
 import lk.tnm.eshop.model.CartItem;
+import lk.tnm.eshop.model.Product;
 
 
 public class CartFragment extends Fragment {
@@ -29,9 +36,12 @@ public class CartFragment extends Fragment {
 
     private FragmentCartBinding binding;
 
+    private List<CartItem> cartItems = new ArrayList<>();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
 
         binding = FragmentCartBinding.inflate(inflater, container, false);
         return binding.getRoot();
@@ -44,7 +54,7 @@ public class CartFragment extends Fragment {
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        if (firebaseAuth.getCurrentUser() != null){
+        if (firebaseAuth.getCurrentUser() != null) {
 
             String uid = firebaseAuth.getCurrentUser().getUid();
 
@@ -53,17 +63,61 @@ public class CartFragment extends Fragment {
                     .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                         @Override
                         public void onSuccess(QuerySnapshot qds) {
-                            if (!qds.isEmpty()){
-                                List<CartItem> cartItems = qds.toObjects(CartItem.class);
+                            if (!qds.isEmpty()) {
+                                cartItems = new ArrayList<>();
+
+                                for (DocumentSnapshot documentSnapshot : qds.getDocuments()) {
+                                    CartItem cartItem = documentSnapshot.toObject(CartItem.class);
+                                    if (cartItem != null) {
+                                        cartItem.setDocumentId(documentSnapshot.getId());
+                                        cartItems.add(cartItem);
+                                    }
+                                }
+
+
+//                                cartItems = qds.toObjects(CartItem.class);
 
                                 LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
                                 binding.cartCartItem.setLayoutManager(layoutManager);
 
-                                CartAdapter adapter = new CartAdapter(cartItems, product -> {
+                                CartAdapter adapter = new CartAdapter(cartItems);
+
+                                adapter.setOnQuantityChangeListener(cartItem -> {
+
+                                    String documentId = cartItem.getDocumentId();
+                                    db.collection("users").document(uid)
+                                            .collection("cart").document(documentId)
+                                            .update("quantity", cartItem.getQuantity())
+                                            .addOnSuccessListener(aVoid -> {
+                                                Toast.makeText(getContext(), "Item Updated", Toast.LENGTH_SHORT).show();
+                                            });
+
+
+                                    updateTotal();
+                                });
+
+                                adapter.setOnRemoveListener(position -> {
+
+                                    String documentId = cartItems.get(position).getDocumentId();
+                                    db.collection("users").document(uid)
+                                            .collection("cart").document(documentId).delete().addOnSuccessListener(aVoid -> {
+                                                cartItems.remove(position);
+                                                adapter.notifyItemRemoved(position);
+                                                adapter.notifyItemRangeChanged(position, cartItems.size());
+                                                updateTotal();
+
+                                                Toast.makeText(getContext(), "Item Removed", Toast.LENGTH_SHORT).show();
+
+
+                                            });
 
                                 });
 
-                                binding.cartCartItem.setAdapter(adapter);
+
+
+
+                                    binding.cartCartItem.setAdapter(adapter);
+                                    updateTotal();
 
 
 
@@ -73,6 +127,57 @@ public class CartFragment extends Fragment {
                     });
 
         }
+
+        binding.cartBtnProceed.setOnClickListener(v ->{
+            CheckoutFragment checkoutFragment = new CheckoutFragment();
+            getParentFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container,checkoutFragment)
+                    .addToBackStack(null)
+                    .commit();
+
+        });
+
+    }
+
+    private void updateTotal() {
+        if (cartItems == null || cartItems.isEmpty()) {
+            binding.cartTextTotal.setText(String.format(Locale.US, "LKR %,.2f", 0.00));
+            return;
+        }
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        List<String> productIds = new ArrayList<>();
+        cartItems.forEach(cartItem -> {
+            productIds.add(cartItem.getProductId());
+        });
+
+        db.collection("products").whereIn("productId", productIds).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot qds) {
+                Map<String, Product> productMap = new HashMap<>();
+
+                qds.getDocuments().forEach(ds -> {
+                    Product product = ds.toObject(Product.class);
+                    if (product != null) {
+                        productMap.put(product.getProductId(), product);
+
+                    }
+                });
+
+                double total = 0;
+                for (CartItem cartItem : cartItems) {
+                    Product product = productMap.get(cartItem.getProductId());
+                    if (product != null) {
+                        total += product.getPrice() * cartItem.getQuantity();
+                    }
+
+                }
+                binding.cartTextTotal.setText(String.format(Locale.US, "LKR %,.2f", total));
+
+            }
+        });
 
     }
 }
